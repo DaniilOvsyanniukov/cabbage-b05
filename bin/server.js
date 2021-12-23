132fb024103ffa8f1ba21b0e944ee4e4d26756e9
Даниил Овсянников <ovsuanikov@gmail.com>
2021-12-23 15:41:36 +0200

    fix

--- bin/server.js
+++ bin/server.js
@@ -1,18 +1,10 @@
 const app = require("../app")
 const mongoose = require("mongoose")
+require("dotenv").config()
 
-const {DB_HOST, PORT = 3001} = process.env
-
-;(async () => {
-  try {
-    await mongoose.connect(DB_HOST)
-    console.log("Успешное подключение к базе данных")
-  } catch (err) {
-    console.log(err.message)
-    process.exit(1)
-  }
-})()
-
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+const url = process.env.MONGO_URL
+const PORT = process.env.PORT
+mongoose
+  .connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log("Server up and running!")))
+  .catch(error => console.log(error.message))
